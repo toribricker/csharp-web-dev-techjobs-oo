69012b82bc0a3623b99ab73318f216711420db7c
toribricker <tori.bricker@icloud.com>
2020-12-24 18:23:47 -0600

    uploaded unit test

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -7,12 +7,17 @@ namespace TechJobsOO
         private static int nextId = 1;
         public string Value { get; set; }
 
-        public Location()
+        public Location() 
         {
             Id = nextId;
             nextId++;
         }
 
+        public Location(int Id) : this ()
+        {
+            Value = "";
+            
+        }
         // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
 
         public override bool Equals(object obj)
---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+   public class JobTests
+    {
+      [TestMethod]
+      public void TestSettingJobId()
+        {
+            Job testJob1 = new Job();
+            Job testJob2 = new Job();
+            Assert.IsFalse(testJob1.Id == testJob2.Id);
+            Assert.IsTrue(testJob2.Id - testJob1.Id == 1);
+        }
+    }
+}
